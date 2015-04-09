da6e4b05d965c9497c698bb226954e8f9433e496
Stephen Brennan <smb196@case.edu>
2015-04-09 11:30:56 -0400

    Added some automatically marked safe variables.

--- init.el
+++ init.el
@@ -25,3 +25,19 @@
   "Code directory.")
 
 (org-babel-load-file (expand-file-name "stemacs.org" stemacs-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((company-clang-arguments "-Isrc/" "-Ilibstephen/inc/")
+     (company-clang-arguments "-Iinc/")
+     (company-clang-arguments "-I/home/stephen/repos/cky/libstephen/inc/" "-I/home/stephen/repos/cky/src/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
