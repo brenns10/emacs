eb085eccc7d36a50fcae13df0712cbe665696cc1
Stephen Brennan <smb196@case.edu>
2016-01-14 20:11:53 -0500

    Update play command!

--- quake-mode/quake-mode.el
+++ quake-mode/quake-mode.el
@@ -276,8 +276,8 @@ since your last frag."
   (when (processp quake/current-sound-process)
     (ignore-errors (kill-process quake/current-sound-process)))
   (setq quake/current-sound-process
-        (start-process "quake/sound" "testyoutput" "aplay"
-		       (concat quake/sound-directory sound-file))))
+        (start-process "quake/sound" "testyoutput" "play"
+                       (concat quake/sound-directory sound-file))))
 
 
 
